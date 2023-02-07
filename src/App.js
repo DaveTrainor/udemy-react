410686f07e9581be30b732e45c10eae95a5db9bd
DaveTrainor <davetrainor79@gmail.com>
2023-02-07 16:56:32 +0000

    Lecture 31: States

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Todo from './Components/Todo';
+import Todo from "./Components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Tools</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo tag="react" />
+      <Todo tag="more react" />
+      <Todo tag="react is life" />
     </div>
   );
 }
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,10 +1,23 @@
-function Todo() {
+import { useState } from 'react';
+import Backdrop from "./Backdrop";
+import Modal from "./Modal"
+
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler(){
+    setModalIsOpen(true);
+    }
+
+
   return (
     <div className="card">
-      <h2>HAMMER</h2>
+      <h2>{props.tag}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+      { modalIsOpen && <Modal />}
+      { modalIsOpen && <Backdrop />}
     </div>
   );
 }
